1a9351791c53cc3ea4c9a0101b6023c153856d34
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-12 16:39:16 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_calcolatrice;
+//SOMMA DI NUMERI INTERI E DECIMALI
+Console.WriteLine($"La somma dei numeri è: {helper_CalcoliHelper.sumNumber(5, 8)}");
+Console.WriteLine($"La somma dei numeri decimali è: {helper_CalcoliHelper.sumNumberDouble(12.2,5.12)}");
+
+//DIFFERENZA DI NUMERI INTERI E DECIMALI
+Console.WriteLine($"Il risultato della divisione è: {helper_CalcoliHelper.divisionNumber(10, 5)}");
+Console.WriteLine($"Il risultato della divisione con decimali decimali è: {helper_CalcoliHelper.divisionNumberDouble(10.5, 5)}");
+
+//NUMERI ASSOLUTI INTERI E DECIMALI
+Console.WriteLine($"Il numero assoluto è: {helper_CalcoliHelper.absoluteValue(4)}");
+Console.WriteLine($"Il numero assoluto è: {helper_CalcoliHelper.absoluteValue(0)}");
+Console.WriteLine($"Il numero assoluto è: {helper_CalcoliHelper.absoluteValue(-9)}");
+Console.WriteLine($"Il numero assoluto decimale è: {helper_CalcoliHelper.absoluteValue(9.23)}");
+Console.WriteLine($"Il numero assoluto decimale è: {helper_CalcoliHelper.absoluteValue(-15.23)}");
+
+//NUMERO MINIMO INTERO E DECIMALE
+Console.WriteLine($"Il numero minimo è: {helper_CalcoliHelper.minValue(54, 45)}");
+Console.WriteLine($"Il numero minimo decimale è:  {helper_CalcoliHelper.minValue(5.25, 46.38)}");
+
+//NUMERO MASSIMO INTERO E DECIMALE
+Console.WriteLine($"Il numero massimo è: {helper_CalcoliHelper.maxValue(15, 89)}");
+Console.WriteLine($"Il numero massimo decimale è: {helper_CalcoliHelper.maxValue(46.65, 56.12)}");
+
---
+++ helper CalcoliHelper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class helper_CalcoliHelper
+    {
+
+        public static int sumNumber(int num1, int num2)
+        {
+           int sum = num1 + num2;
+            return sum;
+        }
+
+        public static double sumNumberDouble(double num1, double num2)
+        {
+            double sum = num1 + num2;
+            return sum;
+        }
+        public static int divisionNumber(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+
+        public static double divisionNumberDouble(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+
+        public static int absoluteValue(int num)
+        {
+            if(num >= 0) 
+            {
+                return num;
+            }else
+            {
+                return num * -1;
+            }
+        }
+
+        public static double absoluteValue(double num)
+        {
+            if (num >= 0)
+            {
+                return num;
+            }
+            else
+            {
+                return num * -1;
+            }
+        }
+
+        public static int minValue(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                return num1;  
+            }
+            else if(num2 < num1)
+            {
+                return num2;
+            }
+            else
+            {
+               return num1;
+            }
+        }
+
+        public static double minValue(double num1, double num2)
+        {
+            if (num1 < num2)
+            {
+                return num1;
+            }
+            else if (num2 < num1)
+            {
+                return num2;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+        public static int maxValue(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else if (num2 > num1)
+            {
+                return num2;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+        public static double maxValue(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else if (num2 > num1)
+            {
+                return num2;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+
+
+    }
+}
+
